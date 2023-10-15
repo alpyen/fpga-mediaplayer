cb4973369ec7c2048352bb04e65e19661e5d7ac0
alpyen <53614168+alpyen@users.noreply.github.com>
2023-10-15 12:29:35 +0200

    hdl: SPI flash model and test bench done

--- sim/memory/spi_flash_model.vhdl
+++ sim/memory/spi_flash_model.vhdl
@@ -1,6 +1,6 @@
 -- Functional model of the onboard SPI flash (Macronix / Spansion)
 -- This model only supports the basic READ command
--- For faster simulation try using smaller sizes
+-- For faster simulation try using smaller flash sizes
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -8,9 +8,6 @@ use ieee.numeric_std.all;
 
 library std;
 use std.textio.all;
-
-library work;
-use work.sim_pkg.all;
 
 entity spi_flash_model is
 generic (
@@ -33,40 +30,64 @@ end entity;
 architecture functional of spi_flash_model is
     type memory_t is array (0 to SIZE-1) of std_ulogic_vector(7 downto 0);
     signal memory: memory_t := (others => INIT_VALUE);
+
+    type state_t is (STATE_COMMAND, STATE_ADDRESS, STATE_DATA);
+    signal state, state_next: state_t;
+
+    signal command, command_next: std_ulogic_vector(7 downto 0);
+    signal address, address_next: std_ulogic_vector(23 downto 0);
+    signal data, data_next: std_ulogic_vector(7 downto 0);
+
+    signal data_out, data_out_next: std_ulogic_vector(7 downto 0);
+
+    -- tracks the command, address and data cycles
+    signal counter, counter_next: integer;
+
+    constant READ_COMMAND: std_ulogic_vector(command'range) := std_ulogic_vector(to_unsigned(3, command'length));
+
+    procedure info (message: in string) is
+    begin
+        report "spi_flash_model: " & message severity note;
+    end procedure;
+
+    procedure fail (message: in string) is
+    begin
+        report "spi_flash_model: " & message severity failure;
+    end procedure;
 begin
     init: process
-        function byte_to_sulv (byte: in string(1 to 2)) return std_ulogic_vector is
-            variable sulv: std_ulogic_vector(7 downto 0);
-            variable nibble: integer;
-        begin
-            for i in 1 to 2 loop
-                case byte(i) is
-                    when '0' => nibble := 0;
-                    when '1' => nibble := 1;
-                    when '2' => nibble := 2;
-                    when '3' => nibble := 3;
-                    when '4' => nibble := 4;
-                    when '5' => nibble := 5;
-                    when '6' => nibble := 6;
-                    when '7' => nibble := 7;
-                    when '8' => nibble := 8;
-                    when '9' => nibble := 9;
-                    when 'A' => nibble := 10;
-                    when 'B' => nibble := 11;
-                    when 'C' => nibble := 12;
-                    when 'D' => nibble := 13;
-                    when 'E' => nibble := 14;
-                    when 'F' => nibble := 15;
-                    when others => report "spi_flash_model: Bad byte read: " & byte(i+1) severity failure;
-                end case;
-
-                sulv(4*(2-i)+3 downto 4*(2-i)+0) := std_ulogic_vector(to_unsigned(nibble, 4));
-            end loop;
-
-            return sulv;
-        end function;
-
         procedure load_file is
+            function byte_to_std_ulogic_vector (byte: in string(1 to 2)) return std_ulogic_vector is
+                variable sulv: std_ulogic_vector(7 downto 0);
+                variable nibble: integer;
+            begin
+                for i in 1 to 2 loop
+                    case byte(i) is
+                        when '0' => nibble := 0;
+                        when '1' => nibble := 1;
+                        when '2' => nibble := 2;
+                        when '3' => nibble := 3;
+                        when '4' => nibble := 4;
+                        when '5' => nibble := 5;
+                        when '6' => nibble := 6;
+                        when '7' => nibble := 7;
+                        when '8' => nibble := 8;
+                        when '9' => nibble := 9;
+                        when 'A' => nibble := 10;
+                        when 'B' => nibble := 11;
+                        when 'C' => nibble := 12;
+                        when 'D' => nibble := 13;
+                        when 'E' => nibble := 14;
+                        when 'F' => nibble := 15;
+                        when others => fail("Bad byte read: " & byte(i+1));
+                    end case;
+
+                    sulv(4*(2-i)+3 downto 4*(2-i)+0) := std_ulogic_vector(to_unsigned(nibble, 4));
+                end loop;
+
+                return sulv;
+            end function;
+
             file memory_file: text;
             variable fos: file_open_status;
 
@@ -78,17 +99,19 @@ begin
 
             variable good: boolean;
         begin
-            report "spi_flash_model: Loading init file: """ & INIT_FILE & """.";
+            info("Loading init file: """ & INIT_FILE & """.");
 
             file_open(fos, memory_file, INIT_FILE, read_mode);
-            assert fos = open_ok report "spi_flash_model: Loading init file failed: " & file_open_status'image(fos) & "." severity failure;
+            if fos /= open_ok then
+                fail("Loading init file failed: " & file_open_status'image(fos) & ".");
+            end if;
 
             while not endfile(memory_file) loop
                 readline(memory_file, current_line);
 
                 while current_line'length > 0 loop
                     if current_address = SIZE then
-                        report "spi_flash_model: Init file is bigger than flash size. Aborting!" severity failure;
+                        fail("spi_flash_model: Init file is bigger than flash size. Aborting!");
                     end if;
 
                     read(current_line, byte, good);
@@ -99,20 +122,106 @@ begin
                         assert good and (space = ' ' or space = cr) report "spi_flash_model: Reading bad byte at: " & integer'image(current_address) & "." severity failure;
                     end if;
 
-                    memory(current_address) <= byte_to_sulv(byte);
+                    memory(current_address) <= byte_to_std_ulogic_vector(byte);
                     current_address := current_address + 1;
                 end loop;
             end loop;
 
-            report "spi_flash_model: Flash initialized with " & integer'image(current_address) & " bytes from init file.";
+            info("Flash initialized with " & integer'image(current_address) & " bytes from init file.");
         end procedure;
     begin
-        report "spi_flash_model: Initializing flash with size of " & integer'image(SIZE) & " bytes.";
+        info("spi_flash_model: Initializing flash with size of " & integer'image(SIZE) & " bytes.");
 
         if INIT_FILE'length /= 0 then
             load_file;
         end if;
 
         wait;
+    end process;
+
+    sdo <= data_out(7);
+
+    seq: process (sclk, cs_n)
+    begin
+        if cs_n = '1' then
+            state <= STATE_COMMAND;
+            counter <= 0;
+            data <= (others => 'Z');
+            data_out <= (others => 'Z');
+        -- sdi is latched in on the rising edge of the clock
+        elsif rising_edge(sclk) and cs_n = '0' then
+            state <= state_next;
+            counter <= counter_next;
+            command <= command_next;
+            address <= address_next;
+            data <= data_next;
+        -- sdo is shifted out on the falling edge of the clock
+        elsif falling_edge(sclk) and cs_n = '0' then
+            data_out <= data_out_next;
+        end if;
+    end process;
+
+    fsm: process (state, counter, command, address, data, data_out, state_next, counter_next, command_next, address_next)
+    begin
+        state_next <= state;
+        counter_next <= counter;
+        command_next <= command;
+        address_next <= address;
+        data_next <= data;
+        data_out_next <= data_out;
+
+        case state is
+            when STATE_COMMAND =>
+                -- check for invalid command
+
+                command_next <= command(command'left-1 downto 0) & sdi;
+                counter_next <= counter + 1;
+
+                if counter = command'length - 1 then
+                    state_next <= STATE_ADDRESS;
+                    counter_next <= 0;
+
+                    if state_next = STATE_ADDRESS and command_next /= READ_COMMAND then
+                        fail("Invalid Command: " & integer'image(to_integer(unsigned(command_next))));
+                    end if;
+                end if;
+
+            when STATE_ADDRESS =>
+                -- check for invalid address
+
+                address_next <= address(address'left-1 downto 0) & sdi;
+                counter_next <= counter + 1;
+
+                if counter = address'length - 1 then
+                    state_next <= STATE_DATA;
+                    counter_next <= 0;
+
+                    -- Need to check for the next state first, otherwise address_next
+                    -- might have not been set from the first process iteration.
+                    -- This probably can be done more elegant?
+                    if state_next = STATE_DATA then
+                        data_next <= memory(to_integer(unsigned(address_next)));
+                    end if;
+
+                    if state_next = STATE_DATA and unsigned(address_next) >= SIZE then
+                        fail("Invalid Address: " & integer'image(to_integer(unsigned(address_next))));
+                    end if;
+                end if;
+
+            when STATE_DATA =>
+                data_next <= data(data'left-1 downto 0) & 'Z';
+                data_out_next <= data;
+                counter_next <= counter + 1;
+
+                if counter = data'length - 1 then
+                    address_next <= std_ulogic_vector(to_unsigned(to_integer((unsigned(address) + 1)) mod SIZE, address_next'length));
+                    counter_next <= 0;
+
+                    -- Increase to the next address and/or wrap around the address range
+                    if counter_next = 0 then
+                        data_next <= memory(to_integer(unsigned(address_next)));
+                    end if;
+                end if;
+        end case;
     end process;
 end architecture;
--- sim/memory/spi_flash_model_tb.vhdl
+++ sim/memory/spi_flash_model_tb.vhdl
@@ -6,11 +6,19 @@ entity spi_flash_model_tb is
 end entity;
 
 architecture tb of spi_flash_model_tb is
-    signal sclk, cs_n, sdi, sdo, wp, hold: std_ulogic;
+    constant T: time := 10 ns;
+    signal sim_done: boolean;
+
+    signal FLASH_SIZE: integer := 256;
+
+    signal sclk: std_ulogic := '0';
+    signal cs_n, sdi, sdo: std_ulogic;
+
+    signal data, data_complete: std_ulogic_vector(7 downto 0);
 begin
     dut: entity work.spi_flash_model
     generic map (
-        SIZE      => 8 * 1024, -- 8 KB
+        SIZE      => FLASH_SIZE, -- 256 B
         INIT_FILE => "../../../../../sim/memory/memory_file.dat",
         INIT_VALUE => x"FF"
     )
@@ -19,7 +27,62 @@ begin
         cs_n => cs_n,
         sdi  => sdi,
         sdo  => sdo,
-        wp   => wp,
-        hold => hold
+        wp   => 'U',
+        hold => 'U'
     );
+
+    clk: process
+    begin
+        sclk <= not sclk;
+        wait for T/2;
+
+        if sim_done then
+            wait;
+        end if;
+    end process;
+
+    test: process
+        variable command: std_ulogic_vector(7 downto 0) := std_ulogic_vector(to_unsigned(3, 8));
+        variable current_address: std_ulogic_vector(23 downto 0);
+    begin
+        cs_n <= '1';
+        sdi <= '0';
+
+        wait for 2*T;
+        
+        for address in 0 to FLASH_SIZE / 16 - 1 loop
+            current_address := std_ulogic_vector(to_unsigned(address * 16, current_address'length));
+
+            wait until rising_edge(sclk);
+            cs_n <= '0';
+
+            for i in command'range loop
+                sdi <= command(i);
+                wait until rising_edge(sclk);
+            end loop;
+
+            for i in current_address'range loop
+                sdi <= current_address(i);
+                wait until rising_edge(sclk);
+            end loop;
+
+            for byteblock in 0 to 15 loop
+                for i in data'range loop
+                    -- data is shifted out on the falling edge,
+                    -- but we have to sample it on the rising edge
+                    wait until rising_edge(sclk);
+                    data <= data(data'left-1 downto 0) & sdo;
+                end loop;
+
+                wait for 0 ns;
+                data_complete <= data;
+            end loop;
+
+            cs_n <= '1';
+            wait for 3*T;
+        end loop;
+
+        sim_done <= true;
+        wait;
+    end process;
 end architecture;
