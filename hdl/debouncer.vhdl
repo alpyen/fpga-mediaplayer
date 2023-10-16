7ccc0713b70b7d98010f05f1529d055ccf1ae63d
alpyen <53614168+alpyen@users.noreply.github.com>
2023-10-16 22:20:20 +0200

    hdl: Fixed Debouncer

--- hdl/debouncer.vhdl
+++ hdl/debouncer.vhdl
@@ -21,20 +21,22 @@ architecture arch of debouncer is
     signal samples, samples_next: std_ulogic_vector(1 downto 0);
     signal counter, counter_next: unsigned(integer(ceil(log2(real(COUNT)))) downto 0);
 
-    signal output_next: std_ulogic;
+    signal output_int, output_int_next: std_ulogic;
 
     constant COUNTER_END: unsigned(counter'range) := to_unsigned(COUNT, counter'length);
 begin
+    output <= output_int;
 
-    process (samples, counter)
+    process (samples, counter, input, output_int)
     begin
         counter_next <= counter + 1;
         samples_next <= samples(0) & input;
+        output_int_next <= output_int;
 
         if samples(1) /= samples(0) then
             counter_next <= (others => '0');
         elsif counter >= COUNTER_END then
-            output_next <= samples(1);
+            output_int_next <= samples(1);
         end if;
     end process;
 
@@ -43,7 +45,7 @@ begin
         if rising_edge(clk) then
             samples <= samples_next;
             counter <= counter_next;
-            output <= output_next;
+            output_int <= output_int_next;
         end if;
     end process;
 end architecture;
